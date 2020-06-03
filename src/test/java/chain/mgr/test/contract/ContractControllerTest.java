972cc6804cf719ce0b686839ffe38109e0f1afe1
v_sflkchen <v_sflkchen@webank.com>
2020-06-03 15:58:35 +0800

    fix Copyright 2014-2020

--- src/test/java/chain/mgr/test/contract/ContractControllerTest.java
+++ src/test/java/chain/mgr/test/contract/ContractControllerTest.java
@@ -1,5 +1,5 @@
 /**
- * Copyright 2014-2019 the original author or authors.
+ * Copyright 2014-2020 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
  * in compliance with the License. You may obtain a copy of the License at
@@ -199,7 +199,7 @@ public class ContractControllerTest {
     
     @Test
     public void testCompileContract2() throws Exception {
-        String filePath = "D:\\project\\sol\\HelloWorld.zip";
+        String filePath = "D:\\project\\fisco\\console-master\\contracts\\solidity\\solidity.zip";
         String base64 = CommonUtils.fileToBase64(filePath);
         System.out.println("base64：" + base64);
         
