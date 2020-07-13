1c4e06dc4374a4c7d27fda23ea29defb3eab375a
Hongbin.Yuan <yuanhongbin9090@gmail.com>
2020-07-13 14:27:25 +0800

    upadte db structure

--- script/webase-ddl.sql
+++ script/webase-ddl.sql
@@ -5,10 +5,15 @@ DROP TABLE IF EXISTS tb_chain;
 CREATE TABLE tb_chain (
   chain_id int(11) NOT NULL COMMENT '区块链编号',
   chain_name varchar(120) DEFAULT NULL COMMENT '区块链名称',
-  chain_type tinyint(4) DEFAULT '0' COMMENT '类型（ 0-非国密 1-国密）',
-  description varchar(1024) COMMENT '描述',
+  chain_type tinyint(4) unsigned NOT NULL DEFAULT '0' COMMENT '加密类型：0，标密；1，国密；默认 0 ',
+  description varchar(512) COMMENT '描述',
   create_time datetime DEFAULT NULL COMMENT '创建时间',
   modify_time datetime DEFAULT NULL COMMENT '修改时间',
+  version varchar(64) NOT NULL DEFAULT '' COMMENT '创建链时选择的镜像版本',
+  consensus_type varchar(16) NOT NULL DEFAULT 'pbft' COMMENT '共识算法 pbft,raft',
+  storage_type varchar(16) NOT NULL COMMENT '存储类型（支持：RocksDB, LevelDB, MySQL）',
+  chain_status tinyint(8) unsigned NOT NULL DEFAULT '0' COMMENT '链状态',
+  webase_sign_addr varchar(255) NOT NULL DEFAULT '127.0.0.1:5004' COMMENT 'WeBASE-Sign 的访问地址',
   PRIMARY KEY (chain_id),
   UNIQUE KEY unique_name (chain_name)
 ) ENGINE=InnoDB CHARSET=utf8 COMMENT='区块链信息表';
@@ -43,8 +48,19 @@ CREATE TABLE IF NOT EXISTS tb_front (
     description varchar(1024) COMMENT '描述',
     create_time datetime DEFAULT NULL COMMENT '创建时间',
     modify_time datetime DEFAULT NULL COMMENT '修改时间',
-    PRIMARY KEY (front_id),
-    UNIQUE KEY unique_chain_node (chain_id,node_id)
+    front_status int(11) DEFAULT 1 COMMENT '前置服务状态：0，未创建；1，停止；2，启动；',
+    agency_id int(10) unsigned DEFAULT '0' COMMENT '所属机构 ID',
+    host_id int(10) unsigned DEFAULT '0' COMMENT '所属主机',
+    host_index int(6) DEFAULT '0' COMMENT '一台主机可能有多个节点。表示在主机中的编号，从 0 开始编号',
+    version varchar(64) DEFAULT '' COMMENT '运行的镜像版本标签',
+    container_name varchar(255) DEFAULT '' COMMENT 'Docker 启动的容器名称',
+    jsonrpc_port int(6) DEFAULT '8545' COMMENT 'jsonrpc 端口',
+    p2p_port int(6) DEFAULT '30303' COMMENT 'p2p 端口',
+    channel_port int(6) DEFAULT '20200' COMMENT 'channel 端口',
+    chain_name varchar(64) DEFAULT '' COMMENT '所属链名称，冗余字段',
+    PRIMARY KEY (`front_id`),
+    UNIQUE KEY unique_chainid_nodeid (chain_id,node_id),
+    UNIQUE KEY `unique_chainid_agencyid_frontip_frontport` (`chain_id`, `agency_id`,`front_ip`,`front_port`)
 ) ENGINE=InnoDB AUTO_INCREMENT=200001 DEFAULT CHARSET=utf8 COMMENT='前置服务信息表';
 
 
@@ -58,6 +74,7 @@ CREATE TABLE IF NOT EXISTS tb_front_group_map (
     group_id int(11) NOT NULL COMMENT '群组编号',
     create_time datetime DEFAULT NULL COMMENT '创建时间',
     modify_time datetime DEFAULT NULL COMMENT '修改时间',
+    front_status tinyint(8) unsigned NOT NULL DEFAULT '0' COMMENT '节点状态: 0 游离；1 共识；2 观察',
     PRIMARY KEY (map_id),
     unique unique_chain_front_group (chain_id,front_id,group_id)
 ) ENGINE=InnoDB AUTO_INCREMENT=300001 DEFAULT CHARSET=utf8 COMMENT='前置群组映射表';
@@ -106,3 +123,38 @@ CREATE TABLE IF NOT EXISTS tb_contract (
     PRIMARY KEY (contract_id),
     UNIQUE KEY uk_group_path_name (chain_id,group_id,contract_path,contract_name)
 ) ENGINE=InnoDB AUTO_INCREMENT=400001 DEFAULT CHARSET=utf8 COMMENT='合约表';
+
+
+-- ----------------------------
+-- Table structure for tb_host
+-- ----------------------------
+CREATE TABLE `tb_host` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT COMMENT '自增长 ID',
+  `agency_id` int(10) unsigned NOT NULL DEFAULT '1' COMMENT '所属机构 ID',
+  `agency_name` varchar(64) DEFAULT NULL COMMENT '所属机构名称，冗余字段',
+  `chain_id` int(11) NOT NULL COMMENT '所属区块链编号',
+  `chain_name` varchar(120) DEFAULT NULL COMMENT '区块链名称',
+  `ip` varchar(16) NOT NULL COMMENT '主机IP',
+  `ssh_user` varchar(64) NOT NULL DEFAULT 'root' COMMENT 'SSH 登录账号',
+  `ssh_port` int(10) unsigned NOT NULL DEFAULT '22' COMMENT 'SSH 端口',
+  `root_dir` varchar(255) NOT NULL DEFAULT '/opt/fisco-bcos' COMMENT '主机存放节点配置文件的根目录，可能存放多个节点配置',
+  `docker_port` int(10) unsigned NOT NULL DEFAULT '2375' COMMENT 'Docker demon 的端口',
+  `status` tinyint(8) unsigned NOT NULL DEFAULT '0' COMMENT '主机状态',
+  `create_time` datetime NOT NULL COMMENT '创建时间',
+  `modify_time` datetime NOT NULL COMMENT '最近一次更新时间',
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `unq_chainid_agencyid_ip` (`chain_id`,`agency_id`,`ip`) USING BTREE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='物理主机信息';
+
+
+CREATE TABLE `tb_agency` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT COMMENT '自增长 ID',
+  `agency_name` varchar(64) NOT NULL COMMENT '机构名称',
+  `agency_desc` varchar(1024) DEFAULT '' COMMENT '机构描述信息',
+  `chain_id` int(10) unsigned NOT NULL DEFAULT '0' COMMENT '所属链 ID',
+  `chain_name` varchar(64) DEFAULT '' COMMENT '所属链名称，冗余字段',
+  `create_time` datetime NOT NULL COMMENT '创建时间',
+  `modify_time` datetime NOT NULL COMMENT '最近一次更新时间',
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `uniq_chain_id_agency_name` (`chain_id`,`agency_name`) USING BTREE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='机构信息表';
