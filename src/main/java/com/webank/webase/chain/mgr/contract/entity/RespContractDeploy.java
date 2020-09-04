29a0f16824e4930fc4a808a9b0d7fd818378ea63
Hongbin.Yuan <yuanhongbin9090@gmail.com>
2020-09-04 14:53:17 +0800

    only return contract address when deploy'

--- src/main/java/com/webank/webase/chain/mgr/contract/entity/RespContractDeploy.java
+++ src/main/java/com/webank/webase/chain/mgr/contract/entity/RespContractDeploy.java
@@ -1,7 +1,5 @@
 package com.webank.webase.chain.mgr.contract.entity;
 
-import java.util.List;
-
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -14,21 +12,5 @@ import lombok.extern.slf4j.Slf4j;
 @Data
 @NoArgsConstructor
 public class RespContractDeploy {
-    private String transactionHash;
-    private long transactionIndex;
-    private String blockHash;
-    private long blockNumber;
-    private long gasUsed;
     private String contractAddress;
-    private String root;
-    private String status;
-    private String message;
-    private String from;
-    private String to;
-    private String input;
-    private String output;
-    private List<String> logs = null;
-    private String logsBloom;
-    private String txProof;
-    private String receiptProof;
 }
