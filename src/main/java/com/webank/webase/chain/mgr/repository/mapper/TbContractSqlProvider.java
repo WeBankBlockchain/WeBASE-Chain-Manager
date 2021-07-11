70bb386b43564ae661a0dfc080ff10eb4d235e60
wusiq <dwusiq@163.c0m>
2021-07-11 11:28:09 +0800

    fix: revert BaseQueryParam.java

--- src/main/java/com/webank/webase/chain/mgr/repository/mapper/TbContractSqlProvider.java
+++ src/main/java/com/webank/webase/chain/mgr/repository/mapper/TbContractSqlProvider.java
@@ -70,7 +70,7 @@ public class TbContractSqlProvider {
 
         // add pagination for mysql with limit clause
         StringBuilder sqlBuilder = new StringBuilder(sql.toString());
-        if (param != null && (param.getStart() > -1 || param.getPageSize() > -1)) {
+        if (param != null && ((param.getStart() !=null && param.getStart() > -1) || (param.getPageSize() !=null &&param.getPageSize() > -1))) {
             sqlBuilder.append(" limit ");
             if (param.getStart() > -1 && param.getPageSize() > -1) {
                 sqlBuilder.append(param.getStart()).append(",").append(param.getPageSize());
