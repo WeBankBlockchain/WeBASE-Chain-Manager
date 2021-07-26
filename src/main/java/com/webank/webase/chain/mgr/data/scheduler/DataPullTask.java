a56460452f15d6baa58167a56304519dbca6b38f
CodingCattwo <847701726@qq.com>
2021-07-26 16:25:05 +0800

    add toggle switch

--- src/main/java/com/webank/webase/chain/mgr/data/scheduler/DataPullTask.java
+++ src/main/java/com/webank/webase/chain/mgr/data/scheduler/DataPullTask.java
@@ -11,7 +11,7 @@
  * or implied. See the License for the specific language governing permissions and limitations under
  * the License.
  */
-package com.webank.webase.chain.mgr.scheduler;
+package com.webank.webase.chain.mgr.data.scheduler;
 
 import com.webank.webase.chain.mgr.group.GroupService;
 import com.webank.webase.chain.mgr.base.enums.DataStatus;
