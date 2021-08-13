d916dd23d6d0809461b234e6230da4435fc63c91
CodingCattwo <847701726@qq.com>
2021-08-13 17:55:30 +0800

    fix sql & delete

--- src/main/java/com/webank/webase/chain/mgr/repository/mapper/TbContractPathMapper.java
+++ src/main/java/com/webank/webase/chain/mgr/repository/mapper/TbContractPathMapper.java
@@ -22,9 +22,7 @@ import org.apache.ibatis.type.JdbcType;
 public interface TbContractPathMapper {
 
     @Delete({
-        "select",
-        TbContractPathSqlProvider.ALL_COLUMN_FIELDS,
-        "from tb_contract_path",
+        "delete from tb_contract_path",
         "where chain_id = #{chainId,jdbcType=INTEGER} and group_id = #{groupId,jdbcType=INTEGER}",
         "and contract_path = #{contractPath}"
     })
